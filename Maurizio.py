f60cab0aba0319c08913b58f5cd90c3226605c70
Vitus61 <62012933+Vitus61@users.noreply.github.com>
2025-07-14 23:42:16 +0200

    Update Maurizio.py

--- Maurizio.py
+++ Maurizio.py
@@ -979,8 +979,8 @@ class CabinaMTBT:
 
         # DATI PROTEZIONI ESTRATTI
         # Estrai le correnti di taratura dalle tue protezioni esistenti
-        I_rele_51_mt = I_mt * 1.25  # Soglia temporizzata MT
-        I_rele_50_mt = I_mt * 20  # Soglia istantanea MT
+        I_rele_51_mt = I_mt * 8.6  # Soglia temporizzata MT
+        I_rele_50_mt = I_mt * 21  # Soglia istantanea MT
 
         # Interruttore BT - estrai dalla stringa
         I_int_bt_str = prot_bt["interruttore_generale"].split("A")[0]
